8d17f77c48657b19633eb18019ebce4c8cc98a5f
Pedro Henrique <167327447+PedroHenriqueSantosGonc@users.noreply.github.com>
2026-09-09 15:45:08 -0300

    Continue

---
+++ quiz/src/components/Questions.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react"
+import { QuizContext } from "../context/quiz"
+
+const Questions = () => {
+
+    const [quizState, dispatch] = useContext(QuizContext)
+
+    const currentQuestion = quizState.questions[quizState.currentQuestion]
+
+    return (
+        <div id="question">
+            <p>Pergunta {quizState.currentQuestion} de {quizState.questions.length}</p>
+            <h2>{currentQuestion.question}</h2>
+            <div className="options-container">
+                <p>Opções</p>
+            </div>
+            <button>Continuar</button>
+        </div>
+    )
+}
+
+export default Questions
